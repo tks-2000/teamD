c6f426885f6ea798d735dece1bdbbf404336e066
Shiba-takashi <kbc20b21@stu.kawahara.ac.jp>
2021-06-30 16:27:10 +0900

    プレイヤーが動いていないときに、ダッシュボタンを押していたらスタミナが回復しない不具合を修正。

--- GameTemplate/Game/Player.h
+++ GameTemplate/Game/Player.h
@@ -32,10 +32,14 @@ public:
 	/// @return プレイヤーの座標
 	Vector3 GetPosition() { return m_position; }
 
+	/// @brief キック可能か判定
 	void IsKick();
 
 	/// @brief ボールを蹴る処理
 	void KickBall();
+
+	/// @brief ダッシュ可能か判定
+	bool IsDash();
 
 	/// @brief ボールとの距離を計算
 	void BallDistanceCalculation();
