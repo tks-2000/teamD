6bde3f53e6f26ad9b4963370eadc890f2608ec7a
Shiba-takashi <kbc20b21@stu.kawahara.ac.jp>
2021-07-10 21:51:08 +0900

    アイテムの動きを追加。リザルトの順位表示のされ方を変更。

--- GameTemplate/Game/Item.h
+++ GameTemplate/Game/Item.h
@@ -23,16 +23,19 @@ public:
 	enum enItemType {
 		enAttackUp,
 		enGuardUp,
-		enSpeedUp
+		enSpeedUp,
+		enItemNum
 	};
 
 	void SetItem(enItemType itemType) { m_itemState = itemType; ItemModelNewGO(); m_newGoFlag = true; }
 
 private:
 	Vector3 m_position = Vector3::Zero;
+	float m_posHeight = 0.0f;
 	Vector3 m_toPos[PLAYER_NUM] = { Vector3::Zero ,Vector3::Zero ,Vector3::Zero ,Vector3::Zero };
 	float m_distance[PLAYER_NUM] = { 0.0f };
 	Quaternion m_qRot = Quaternion::Identity;
+	float m_angle = 0.0f;
 	SkinModelRender* m_skinModelRender = nullptr;
 	int m_myNo = 4;
 	Player* m_player[PLAYER_NUM] = { nullptr };
@@ -40,6 +43,8 @@ private:
 	Objects* m_objects = nullptr;
 	enItemType m_itemState = enAttackUp;
 	bool m_newGoFlag = false;
+
+	bool m_moveChange = false;
 
 	void ItemModelNewGO();
 
--- GameTemplate/Game/Result.h
+++ GameTemplate/Game/Result.h
@@ -15,15 +15,23 @@ private:
 	FontRender* m_selectFont[SELECT_COMMAND_NUM] = { nullptr };
 	FontRender* m_rankingFont[PLAYER_NUMBER] = { nullptr };
 	FontRender* m_playerNameFont[PLAYER_NUMBER] = { nullptr };
-
+	bool m_playerFontSet[PLAYER_NUMBER] = { false };
+	bool m_moveFlag[PLAYER_NUMBER] = { false };
+	bool m_moveEndFlag = false;
+	Vector2 m_plFontPos[PLAYER_NUMBER] = { Vector2::Zero,Vector2::Zero ,Vector2::Zero ,Vector2::Zero };
+	Vector2 m_movePos[PLAYER_NUMBER] = { Vector2::Zero,Vector2::Zero ,Vector2::Zero ,Vector2::Zero };
 	GameDirector* m_gameDirector = nullptr;
 	Score* m_score = nullptr;
 
+	void PlayerFontMove(int num);
+
+	void SelectCommandNewGO();
 public:
 	Result();
 	~Result();
 	bool Start();
 	void Update();
+	bool IsMoveEnd() { return m_moveEndFlag; }
 
 	int GetSelectCommand() { return m_selectNum; }
 };
